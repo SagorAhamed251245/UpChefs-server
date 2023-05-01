6217753addbff36bceff9e3a82050a95180e157b
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-02 01:10:30 +0600

    all image link added

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.get('/', (req, res) => {
 app.get('/chefs',(req, res) => {
     res.send(chefDetails) 
 })
+ app.get('/chefs/:id', (req , res)=> {
+    const id = req.params.id;
+    const item = chefDetails?.find(p => p.id == id)
+    res.send(item)
+ })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
