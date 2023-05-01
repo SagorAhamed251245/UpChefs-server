ae193d492af89518945abebb65930905be6bcc53
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-01 23:56:39 +0600

    json file setup for chefDetailes

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const cors = require('cors');
 
 app.use(cors());
 
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
