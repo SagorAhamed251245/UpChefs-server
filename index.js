b2db3ab6c630c5ceb19fd433f42c8f31f1d4940b
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-02 14:41:45 +0600

    server setup done

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const express = require("express");
-
+require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-
+const chefDetails = require('./chefs.json');
+const recipes = require('./recipes.json');
 app.use(cors());
 
-const chefDetails = require("./data/ChefDetails .json")
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 app.get('/chefs',(req, res) => {
     res.send(chefDetails) 
-})
+});
  app.get('/chefs/:id', (req , res)=> {
     const id = req.params.id;
     const item = chefDetails?.find(p => p.id == id)
+    res.send(item)
+ });
+ app.get('/recipes', (req, res) => {
+    res.send(recipes)
+ })
+ app.get('/recipes/:id',(req, res) => {
+    const id = req.params.id;
+    const item = recipes?.find(p => p.id == id)
     res.send(item)
  })
 
